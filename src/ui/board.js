611ad8ebc24e44fc59c725757013a30fd6e1d751
Omkar Bhale <omkarbhale001@gmail.com>
2024-02-10 15:59:06 +0530

    Update board to use a number generator

--- src/ui/board.js
+++ src/ui/board.js
@@ -1,17 +1,39 @@
-import { log } from "../logger.js";
 import { Tile } from "./tile.js";
 
 export class Board {
     constructor(rows, cols, container) {
-        this.container = container;
-        this.tiles = [];
-        for (let i = 0; i < rows; i++) {
+        this._rows = rows;
+        this._cols = cols;
+        this._container = container;
+        this._tiles = []; // 2D array of tiles
+    }
+
+    setNumberGenerator(numberGenerator) {
+        if (Array.isArray(numberGenerator)) {
+            this._numberGenerator = () => {
+                const index = Math.floor(Math.random() * numberGenerator.length);
+                return numberGenerator[index];
+            }
+            return;
+        }
+
+        if (typeof numberGenerator === "function") {
+            this._numberGenerator = numberGenerator;
+            return;
+        }
+
+        throw new Error("Number generator must be an array or a function");
+    }
+
+    populateTiles() {
+        if (!this._numberGenerator) throw new Error("Number generator not set");
+        for (let i = 0; i < this._rows; i++) {
             const row = [];
-            for (let j = 0; j < cols; j++) {
-                const tile = new Tile(i, j, container, this);
+            for (let j = 0; j < this._cols; j++) {
+                const tile = new Tile(i, j, this._numberGenerator(), this._container, this);
                 row.push(tile);
             }
-            this.tiles.push(row);
+            this._tiles.push(row);
         }
     }
 }
