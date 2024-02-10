37badf890ed1fb03cd5d59d289457f5dc1dd2b5a
Omkar Bhale <omkarbhale001@gmail.com>
2024-02-10 15:58:50 +0530

    Removed logger

--- src/logger.js
+++
@@ -1,9 +0,0 @@
-
-export const log = function(type, message) {
-    if (typeof message === 'string') {
-        console.log(`[${type.toUpperCase()}] ${message}`);
-    } else {
-        console.log(`[${type.toUpperCase()}]:`);
-        console.log(message);
-    }
-};
