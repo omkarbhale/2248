9a29814c220145e9490b774d0d2d3db28bf0e0e6
Omkar Bhale <omkarbhale001@gmail.com>
2024-03-27 20:09:38 +0530

    Connection visuals

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
-import { rows, cols } from "./constants.js"
+import { ROWS, COLS } from "./constants.js"
 import { Board } from "./ui/board.js";
-
-// async function gameLoop() {
-//     tile.update();
-//     requestAnimationFrame(gameLoop);
-// }
+import { Connection } from "./ui/connection.js";
 
 async function main() {
     console.log("info", "Starting");
 
     const container = document.querySelector(".container")
-    const board = new Board(rows, cols, container);
+    const board = new Board(ROWS, COLS, container);
+    board.setNumberGenerator(() => { return undefined; });
+    board.populateTiles();
+    const c = new Connection(board._tiles[2][2], board._tiles[1][2], container);
+    const c1 = new Connection(board._tiles[5][2], board._tiles[1][4], container);
 
     window.board = board;
+    window.c = c;
+
+    board._tiles[2][1].row = 8;
 }
 main();
--- src/ui/tile.js
+++ src/ui/tile.js
@@ -11,6 +11,12 @@ export class Tile {
         this._element.style.setProperty("--j", col);
         this._element.style.setProperty("--bg-color", this.randomColor());
         container.appendChild(this._element);
+
+        this._events = {
+            "positionchange": [
+                // {callback, once, args, originalCallback, applied}
+            ],
+        }
     }
 
     randomColor() {
@@ -23,24 +29,100 @@ export class Tile {
         })`
     }
 
+    /**
+     * 
+     * @param {string} event Event name
+     * @param {Function} callback Callback function
+     * @param {boolean?} once Once
+     * @param {any[]} args Args to be passed to the callback
+     */
+    on(event, callback_, once=false, ...args) {
+        if (!this._events.hasOwnProperty(event)) {
+            throw new Error(`Event ${event} is not supported`);
+        }
+        if (!args) args = [];
+        this._events[event].push({
+            callback: (e) => {
+                callback_(e, ...args);
+            },
+            once,
+            args,
+            originalCallback: callback_,
+            applied: false,
+        });
+        this._applyEvents(true);
+    }
+
+    off() {
+        throw new Error("Not implemented off")
+    }
+
+    /**
+     * 
+     * @param {boolean} toApply Whether to add to remove
+     */
+    _applyEvents(toApply) {
+        for (const event in this._events) {
+            for (const { callback, once, args, applied } of this._events[event]) {
+                if (toApply) {
+                    if (applied) continue;
+
+                    switch (event) {
+                        case "positionchange":
+                            // Don't need to apply positionchange
+                        break;
+                        case "click":
+                            // TODO
+                        break;
+                        default:
+                            throw new Error("Unreachable code");
+                    }
+                }
+            }
+        }
+    }
+
+    get row() {
+        return this._row;
+    }
     set row(r) {
+        const dr = r - this._row;
         this._row = r;
         this._element.style.setProperty("--i", this._row);
+        this._onPositionChanged(dr, 0);
     }
 
+    get col() {
+        return this._col;
+    }
     set col(c) {
-        this._col = c;
-        this._element.style.setProperty("--j", this._col);
+       const dc = c - this._col;
+       this._col = c;
+       this._element.style.setProperty("--j", this._col);
+       this._onPositionChanged(0, dc);
     }
 
-    remove(onRemove) {
-        if (!this._element.isConnected) {
-            throw new Error("Tried to remove a tile which was already removed");
+    /**
+     * 
+     * @param {number} dr Delta row
+     * @param {number} dc Delta column
+     */
+    _onPositionChanged(dr, dc) {
+        for (const key in this._events["positionchange"]) {
+            const { callback, once, args, applied } = this._events["positionchange"][key];
+            callback({ target: this, row: this._row, col: this._col, drow: dr, dcol: dc }, ...args);
         }
-        this._element.addEventListener("animationend", (e) => {
-            e.target.remove();
-            onRemove ? onRemove(this._row, this._col) : undefined;
-        }, { once: true });
-        this._element.classList.add("kill");
+    }
+
+    remove() {
+        return new Promise((res, rej) => {
+            if (!this._element.isConnected) {
+                rej("Tried to remove a tile which was already removed");
+            }
+            this._element.addEventListener("animationend", (e) => {
+                res(this._row, this._col);
+            }, { once: true });
+            this._element.classList.add("kill");
+        })
     }
 }
