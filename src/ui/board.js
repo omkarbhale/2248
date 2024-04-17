52976fbfd04b4ea665483f43d0286e3dbd6bccd8
Omkar Bhale <omkarbhale001@gmail.com>
2024-04-17 21:35:06 +0530

    Browser compatibility fix

--- src/ui/board.js
+++ src/ui/board.js
@@ -39,7 +39,7 @@ export class Board {
                 numbers.push(this._tiles[i][j].value);
             }
         }
-        const toPickFrom = new Set(numbers).values().toArray().sort((a, b) => a-b).slice(0, 3);
+        const toPickFrom = [...new Set(numbers).values()].sort((a, b) => a-b).slice(0, 3);
         console.log(toPickFrom);
         this.setNumberGenerator(toPickFrom);
     }
